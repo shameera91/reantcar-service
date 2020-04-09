ccbf71dd9f9c1a1004ad4918513a0550d7d63371
Shameera Anuranga <Shameera.A@synergenhealth.com>
2020-04-09 15:03:29 +0530

    add validation for ListConvertor

--- src/main/java/com/car/rentservice/common/converters/StringListConverter.java
+++ src/main/java/com/car/rentservice/common/converters/StringListConverter.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created By Shameera.A on 3/28/2020
@@ -17,11 +19,17 @@ public class StringListConverter implements AttributeConverter<List<String>, Str
 
     @Override
     public String convertToDatabaseColumn(List<String> stringList) {
+        if(stringList.isEmpty()){
+            return "";
+        }
         return String.join(SPLIT_CHAR, stringList);
     }
 
     @Override
     public List<String> convertToEntityAttribute(String string) {
+        if(Objects.isNull(string)){
+            return Arrays.asList();
+        }
         return Arrays.asList(string.split(SPLIT_CHAR));
     }
 }
